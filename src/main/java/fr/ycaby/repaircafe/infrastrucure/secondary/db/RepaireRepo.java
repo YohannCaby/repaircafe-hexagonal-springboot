d5a707c6c006cd0bfc6a37d6c56ede9411d28d45
Yohann CABY <ycaby@solocal.com>
2024-06-11 22:52:43 +0200

    Implementation infra Repair repo

--- src/main/java/fr/ycaby/repaircafe/infrastrucure/secondary/db/RepaireRepo.java
+++ src/main/java/fr/ycaby/repaircafe/infrastrucure/secondary/db/RepaireRepo.java
@@ -3,6 +3,10 @@ package fr.ycaby.repaircafe.infrastrucure.secondary.db;
 import fr.ycaby.repaircafe.core.model.Device;
 import fr.ycaby.repaircafe.core.model.Repair;
 import fr.ycaby.repaircafe.core.port.persistence.port.RepairRepoPort;
+import fr.ycaby.repaircafe.infrastrucure.secondary.db.entities.RepairEntity;
+import fr.ycaby.repaircafe.infrastrucure.secondary.db.entities.RepairPK;
+import fr.ycaby.repaircafe.infrastrucure.secondary.db.mapper.RepairEntityMapper;
+import fr.ycaby.repaircafe.infrastrucure.secondary.db.repo.RepairJpaRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,28 +14,49 @@ import java.util.List;
 @Service
 public class RepaireRepo implements RepairRepoPort {
 
+    private final RepairJpaRepo repairJpaRepo;
+    private final RepairEntityMapper mapper;
+
+    public RepaireRepo(RepairJpaRepo repairJpaRepo, RepairEntityMapper mapper) {
+        this.repairJpaRepo = repairJpaRepo;
+        this.mapper = mapper;
+    }
+
     @Override
     public List<Repair> getFrom(Device device) {
-        return List.of();
+        return repairJpaRepo.findByDeviceSerialNumber(device.getDeviceSerialNumber()).stream().map(mapper::toDomain).toList();
     }
 
     @Override
     public boolean isFromExist(Device device, Repair o) {
-        return false;
+        return repairJpaRepo.existsById(RepairPK.builder()
+                .deviceSerialNumber(device.getDeviceSerialNumber())
+                .date(o.getDate())
+                .build());
     }
 
     @Override
     public Repair updateFrom(Device device, Repair o) {
-        return null;
+        return saveOrUptade(device, o);
     }
 
     @Override
     public Repair createFrom(Device device, Repair o) {
-        return null;
+        return saveOrUptade(device, o);
     }
 
     @Override
     public Repair removeFrom(Device device, Repair o) {
-        return null;
+        RepairEntity entity = mapper.toEntity(o);
+        entity.getId().setDeviceSerialNumber(device.getDeviceSerialNumber());
+        repairJpaRepo.delete(entity);
+        return o;
     }
+
+    private Repair saveOrUptade(Device device, Repair o) {
+        RepairEntity entity = mapper.toEntity(o);
+        entity.getId().setDeviceSerialNumber(device.getDeviceSerialNumber());
+        return mapper.toDomain(repairJpaRepo.save(entity));
+    }
+
 }
--- src/main/java/fr/ycaby/repaircafe/infrastrucure/secondary/db/repo/RepairJpaRepo.java
+++ src/main/java/fr/ycaby/repaircafe/infrastrucure/secondary/db/repo/RepairJpaRepo.java
@@ -2,8 +2,10 @@ package fr.ycaby.repaircafe.infrastrucure.secondary.db.repo;
 
 import fr.ycaby.repaircafe.infrastrucure.secondary.db.entities.RepairEntity;
 import fr.ycaby.repaircafe.infrastrucure.secondary.db.entities.RepairPK;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RepairJpaRepo extends CrudRepository<RepairEntity, RepairPK> {
 
+    List<RepairEntity> findByDeviceSerialNumber(String deviceSerialNumber);
 }
